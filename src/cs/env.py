c7f9869111c6705f98e6063761bce000db847523
SAHIL15KP <sahil15kp@gmail.com>
2024-10-31 18:16:35 +0530

    connected my project with postgress database through kubernets cluster of akamai database

--- src/cs/env.py
+++ src/cs/env.py
@@ -1,15 +1,23 @@
 import pathlib
 from decouple import Config , RepositoryEnv
+from functools import lru_cache
+
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 ROOT_DIR = BASE_DIR.parent
 ENV_FILE_NAME = ".env"
 ENV_BASE_DIR = BASE_DIR / ENV_FILE_NAME
+ENV_ROOT_DIR = ROOT_DIR / ENV_FILE_NAME
 
+@lru_cache
 def get_config():
     if ENV_BASE_DIR.exits():
         return Config(
             RepositoryEnv(str(ENV_BASE_DIR))
+        )
+    if ENV_ROOT_DIR.exits():
+        return Config(
+            RepositoryEnv(str(ENV_ROOT_DIR))
         )
     from decouple import config 
     return config 
