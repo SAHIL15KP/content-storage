3ecf4fdd64cd4b21c1f75c05c856d37e74918c12
SAHIL15KP <sahil15kp@gmail.com>
2024-12-26 12:38:32 +0530

    merged with original

--- src/manage.py
+++ src/manage.py
@@ -6,20 +6,16 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
-=======
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cs.settings")
->>>>>>> 7aaa06d17b82687db3714004d4251da1285ce34f
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cs.settings")
+try:
+    from django.core.management import execute_from_command_line
+except ImportError as exc:
+    raise ImportError(
+        "Couldn't import Django. Are you sure it's installed and "
+        "available on your PYTHONPATH environment variable? Did you "
+        "forget to activate a virtual environment?"
+    ) from exc
+execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
